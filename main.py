bf26f8c7d028226cc1e07e5e5555182a50dd4496
njbizzle <33376160+njbizzle@users.noreply.github.com>
2018-06-05 18:13:11 -0700

    Update main.py

--- main.py
+++ main.py
@@ -29,12 +29,16 @@ drawLoop = True
 clock = pygame.time.Clock()
 poop=pygame.image.load("Poop_Emoji.png")
 fireball=pygame.image.load("Fireball.png")
-
+fireball2=pygame.image.load("Fireball.png")
+fbxVal2=10
 fbxVal=10
 fbx = 560
 fby = random.randint(100,400)
 x = 0
 y = 0
+fbx2=0
+fby2=0
+end=True
 grounded=False
 yVelocity=0
 gravity=1
@@ -54,7 +58,8 @@ while drawLoop == True:
     #update
     clock.tick(60)
     yVelocity+=gravity
-    fbx-=fbxVal
+    fbx2-=fbxVal2-5
+    fbx-=fbxVal-3
     y+=yVelocity
     if  x>560:
         x=-100
@@ -62,16 +67,28 @@ while drawLoop == True:
         x=560
     if y>400:
         y=400
+        grounded = True
+    else:
+        grounded = False
     if fbx < -100:
         fbx = 560
         fby = random.randint(100, 400)
-        grounded=True
+    if fbx2 < -100:
+        fbx2 = 560
+        fby2 = y
+    if fby and fbx == y and y or fby2 and fbx2 == y and y:
+        while end==True:
+            fby=0
+            fbx=0
+            fbx2=0
+            fby2=0
+            y=0
+            x=0
 
     if keyDown (pygame.K_d):
         x += 5
     if keyDown(pygame.K_w) and grounded==True:
-        yVelocity=-30
-        grounded=False
+        yVelocity=-20
     if keyDown(pygame.K_a):
         x -= 5
 
@@ -82,8 +99,6 @@ while drawLoop == True:
     #pygame.draw.circle(screen, RED,(int(x), 20), 50)
     screen.blit(poop, (int(x), int(y)))
     screen.blit(fireball, (int(fbx), int(fby)))
-
-
-
+    screen.blit(fireball2, (int(fbx2), int(fby2)))
 
     pygame.display.flip()
